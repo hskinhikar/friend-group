1326b278bcd4b219736c8dab48a2c77338fe59f8
Hruday Kinhikar <156102552+hskinhikar@users.noreply.github.com>
2024-10-14 10:57:33 +0100

    adding data structure details

--- group.py
+++ group.py
@@ -2,4 +2,9 @@
 
 # Your code to go here...
 
-my_group =
+my_group = my_group = [
+    {"name": "Tom", "age": 12, "job": "developer", "relation": "Gil's friend"},
+    {"name": "Gil", "age": 12, "job": "developer","relation": "Tom's friend, Ben's brother"},
+    {"name": "Ben", "age": 12, "job": "developer","relation": "Harry's colleague, Gils brother"},
+    {"name": "Harry", "age": 12, "job": "developer","relation": "Bens's colleague"}
+]
